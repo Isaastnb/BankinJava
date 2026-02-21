52e6b1731a2e5bdedbe7f1179b57f8746cd9aa3e
Isaastnb <isaqueladeiraesteves@gmail.com>
2026-02-21 18:53:09 -0300

    DataBase update

--- src/main/java/bankin/DataBase.java
+++ src/main/java/bankin/DataBase.java
@@ -13,8 +13,29 @@ public class DataBase  {
     String comando;
 
     public void insert (Account a) throws SQLException {
-        connection = DriverManager.getConnection(url,user,pswd);
-        comando = "INSERT INTO account(name,age,money) VALUES (?,?,?)";
+        connection = DriverManager.getConnection(url, user, pswd);
+
+        // Mudamos o check para buscar os dados reais, não apenas contar
+        String check = "SELECT age, money FROM account WHERE name = ?";
+        comando = "INSERT INTO account(name, age, money) VALUES (?, ?, ?)";
+
+        PreparedStatement checkstmt = connection.prepareStatement(check);
+        checkstmt.setString(1, a.getName());
+        ResultSet rs = checkstmt.executeQuery();
+
+        if (rs.next()) {
+
+            System.out.println("Welcome back, " + a.getName() + "!");
+
+            a.setAge(rs.getInt("age"));
+            a.setMoney(rs.getDouble("money"));
+
+            rs.close();
+            checkstmt.close();
+            connection.close();
+            return;
+        }
+
 
         PreparedStatement stmt = connection.prepareStatement(comando);
 
